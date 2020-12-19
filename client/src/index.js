491329e6a9adca6fbca32b5800a732a8f5241ba8
Diane998 <gutierrez.almiradyan@gmail.com>
2020-12-20 01:35:50 +0800

    Updated .gitignore file

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,7 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
 
-import {
-  ApolloProvider,
-  ApolloClient,
-  InMemoryCache,
-  gql
-} from '@apollo/client';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 import AppContainer from './containers/AppContainer';
 
